0c4dd939c2ab43469b64cdd9b58a3bca6189e604
nvrqt03 <ajktg21@gmail.com>
2021-02-12 09:59:33 -0600

    espresso test passed

--- app/src/androidTest/java/ajmitchell/android/bakingapp2/RecipeActivityBasicTest.java
+++ app/src/androidTest/java/ajmitchell/android/bakingapp2/RecipeActivityBasicTest.java
@@ -1,31 +1,63 @@
 package ajmitchell.android.bakingapp2;
 
 
+import android.view.View;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.Before;
+import org.hamcrest.Matcher;
+
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
+
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class RecipeActivityBasicTest {
 
+    @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule =
             new ActivityScenarioRule<MainActivity>(MainActivity.class);
 
-//    @Before
+    //    @Before
 //    public void init() {
 //        activityScenarioRule.getScenario().onActivity()
 //    }
-    @Rule public void testRecipeItem() {
-        onView(withId(R.id.recipe_list))
-                .perform(click());
 
+    @Test
+    public void testRecipeItem() {
+        onView(withId(R.id.recipe_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id.recipe_name)));
+    }
+
+    public static class MyViewAction {
+
+        public static ViewAction clickChildViewWithId(final int id) {
+            return new ViewAction() {
+                @Override
+                public Matcher<View> getConstraints() {
+                    return null;
+                }
+
+                @Override
+                public String getDescription() {
+                    return "Click on a child view with specified id.";
+                }
+
+                @Override
+                public void perform(UiController uiController, View view) {
+                    View v = view.findViewById(id);
+                    v.performClick();
+                }
+            };
+        }
 
     }
 }
